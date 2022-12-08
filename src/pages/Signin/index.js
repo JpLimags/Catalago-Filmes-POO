67cbfaee2620632edb4e988124beefeb804b3730
Felipe <felipe21fab@gmail.com>
2022-12-07 22:24:54 -0300

    Finished Signin style

--- src/pages/Signin/index.js
+++ src/pages/Signin/index.js
@@ -4,6 +4,7 @@ import Button from "../../components/button"
 import * as C from "./styles"
 import { Link, useNavigate } from "react-router-dom"
 import useAuth from "../../hooks/useAuth"
+import logo from "./assets/Logo.png"
 
 const Signin = () => {
   const { signin } = useAuth()
@@ -31,28 +32,35 @@ const Signin = () => {
 
   return (
     <C.Container>
-      <C.Label>SISTEMA DE LOGIN</C.Label>
+      <C.Left>
+        <h1 style={{color: "white", fontSize: '50px'}}>Faça seu login na plataforma</h1>
+        <img src={logo} alt="Logo do site"></img>
+      </C.Left>
       <C.Content>
-        <Input
-          type="email"
-          placeholder="Digite seu E-mail"
-          value={email}
-          onChange={(e) => [setEmail(e.target.value), setError("")]}
-        />
-        <Input
-          type="password"
-          placeholder="Digite sua Senha"
-          value={senha}
-          onChange={(e) => [setSenha(e.target.value), setError("")]}
-        />
-        <C.labelError>{error}</C.labelError>
-        <Button Text="Entrar" onClick={handleLogin} />
-        <C.LabelSignup>
-          Não tem uma conta?
-          <C.Strong>
-            <Link to="/signup">&nbsp;Registre-se</Link>
-          </C.Strong>
-        </C.LabelSignup>
+        <C.Label>Faça login</C.Label>
+        <C.Form>
+          <Input
+            type="email"
+            placeholder="Digite seu E-mail"
+            value={email}
+            onChange={(e) => [setEmail(e.target.value), setError("")]}
+          />
+          <Input
+            type="password"
+            placeholder="Digite sua Senha"
+            value={senha}
+            onChange={(e) => [setSenha(e.target.value), setError("")]}
+          />
+          <C.labelError>{error}</C.labelError>
+          <Button Text="Entrar" onClick={handleLogin} />
+          <C.LabelSignin>
+            Não tem uma conta?
+            <C.Strong>
+              <Link to="/signup">&nbsp;Registre-se</Link>
+            </C.Strong>
+          </C.LabelSignin>
+        </C.Form>
+        
       </C.Content>
     </C.Container>
   )
